7eedb6328dbad26cd8375e94a9167b47761c4c4b
Monique Emídio de Oliveira <49343419+moniquemidio@users.noreply.github.com>
2026-08-25 22:13:25 -0300

    Create 3 - if_else.c

---
+++ 3 - if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  int num;
+
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &num);
+
+  if (num % 2 == 0) {
+    printf("Numero par.\n");
+  } else {
+    printf("Numero impar.\n");
+  }
+  
+  return 0;
+
+}
